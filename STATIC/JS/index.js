5a692df6c474113e8c4b1fc24b1ef2be97ddecf5
gabbog07 <gabri152007@gmail.com>
2024-10-02 08:55:13 +0200

    esercizio-fine

--- STATIC/JS/index.js
+++ STATIC/JS/index.js
@@ -1,39 +1,84 @@
-// Qui scriviamo il codice JS
-
-// let è una parola chiave che serve a dichiarare delle variabili in JS
-let firstName = prompt("Qual è il tuo nome?")
-
-
-if(firstName === null){
-
+function percentuale(valore, totale = 7) {
+  return (valore / totale) * 100;
 }
 
-// prompt è una funzione che chiede un input all'utente e funziona in maniera del tutto simile a python:
-// - restituisce un valore -> posso fare una assegnazione
-// - di default restituisce una string
+function sanatize(string) {
+  return string.toLowerCase().trim();
+}
 
-// console.log() logga in console il messaggio passato come argomento; non è bloccante
-console.log("Benvenuto " + firstName)
+let nome;
 
-/*
+do {
+  nome = prompt("Inserisci il tuo nome:");
+} while (nome === null || nome.trim() === "");
 
-let myVar = 0
-myVar = myVar + 1
-oppure
-myVar++
-*/
+//variabile che mantiene il punteggio
+let punti = 0;
 
-/*
-    Chiediamo un nome all'utente+
-        - Se l'utente non insersce il nome continuiamo a chiederlo finchè non inserisce il nome
-   
-    Facciamo 7 domande all'utente:
-    - A ciascuna domanda l'utente può rispondere con una parola
-    - Prendere in input la risposta dell'utente
-    - fare un sanitize di base della risposta utente
-    - controllare se la risposta è quella giusta
-    - in caso affermativo incrementare il numero di risposte corrette
-    - in caso negativo mostrare la risposta corretta all'utente
+//prima domanda
+let d1;
+do {
+  d1 = prompt("Qual è la capitale del Brasile?");
+} while (d1 === null || d1.trim() === "");
+d1 = sanatize(d1);
+if (d1 === "brasilia") punti++;
+else alert("errato, la risposta giusta è Brasilia");
 
-    Al termine delle sette domande mostrare con console.log la percentuale di risposte esatte
-*/
+//seconda domanda
+let d2;
+do {
+  d2 = prompt("Chi ha scritto la Divina Commedia?");
+} while (d2 === null || d2.trim() === "");
+d2 = d2.toLowerCase().trim();
+if (d2 === "dante") punti++;
+else alert("errato, la risposta giusta è Dante");
+
+//terza domanda
+let d3;
+do {
+  d3 = prompt("Quanti giorni ci sono in un anno?");
+} while (d3 === null || d3.trim() === "");
+d3 = d3.toLowerCase().trim();
+if (d3 === "365") punti++;
+else alert("errato, la risposta giusta è 365");
+
+//quarta domanda
+let d4;
+do {
+  d4 = prompt("Come si dice ciao in inglese");
+} while (d4 === null || d4.trim() === "");
+d4 = d4.toLowerCase().trim();
+if (d4 === "hello") punti++;
+else alert("errato, la risposta giusta è Hello");
+
+//quinta domanda
+let d5;
+do {
+  d5 = prompt("Qual è il continente più grande del mondo?");
+} while (d5 === null || d5.trim() === "");
+d5 = d5.toLowerCase().trim();
+if (d5 === "russia") punti++;
+else alert("errato, la risposta giusta è Russia");
+
+//sesta domanda
+let d6;
+do {
+  d6 = prompt("Quanti secondi ci sono in un minuto?");
+} while (d6 === null || d6.trim() === "");
+d6 = d6.toLowerCase().trim();
+if (d6 === "60") punti++;
+else alert("errato, la risposta giusta è 60");
+
+//settima domanda
+let d7;
+do {
+  d7 = prompt("Qual è il mese più corto dell anno?");
+} while (d7 === null || d7.trim() === "");
+d7 = d7.toLowerCase().trim();
+if (d7 === "febbraio") punti++;
+else alert("errato, la risposta giusta è Febbraio");
+
+//stampa punteggio finale
+alert("il tuo punteggio finale: " + punti);
+
+console.log("percentuale risposte giuste: " + percentuale);
